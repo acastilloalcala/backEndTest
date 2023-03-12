327b2c0891172005ae0f861a97778761cce53c29
Antonio Castillo <acastillo@beesion.com>
2023-03-12 10:02:03 +0100

    Controlador y main spring

---
+++ src/main/java/com/acastillo/dev/backEndTest/BackEndTestApplication.java
@@ -0,0 +1,13 @@
+package com.acastillo.dev.backEndTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndTestApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/acastillo/dev/backEndTest/controllers/CalculatorController.java
@@ -0,0 +1,35 @@
+package com.acastillo.dev.backEndTest.controllers;
+
+import java.math.BigDecimal;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.acastillo.dev.backEndTest.dto.OperationDetailDto;
+import com.acastillo.dev.backEndTest.services.ICalculatorService;
+import com.acastillo.dev.backEndTest.spec.CalculatorApi;
+
+import io.swagger.annotations.ApiParam;
+
+@RestController
+public class CalculatorController implements CalculatorApi {
+	
+	@Autowired
+	private ICalculatorService calculatorService;
+
+	public ResponseEntity<OperationDetailDto> subtract(@NotNull @ApiParam(value = "", required = true) @Valid @RequestParam(value = "operator1", required = true) BigDecimal operator1,@NotNull @ApiParam(value = "", required = true) @Valid @RequestParam(value = "operator2", required = true) BigDecimal operator2){
+		OperationDetailDto result = calculatorService.subtract(operator1, operator2);
+		return new ResponseEntity<>((result), HttpStatus.OK);
+	}
+	
+	public ResponseEntity<OperationDetailDto> sum(@NotNull @ApiParam(value = "", required = true) @Valid @RequestParam(value = "operator1", required = true) BigDecimal operator1,@NotNull @ApiParam(value = "", required = true) @Valid @RequestParam(value = "operator2", required = true) BigDecimal operator2){
+		OperationDetailDto result = calculatorService.sum(operator1, operator2);
+		return new ResponseEntity<>((result), HttpStatus.OK);
+	}
+}
